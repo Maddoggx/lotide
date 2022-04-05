4ee3a86d1da3e871d5783bcd3819fcf184422f8a
Amera Alleyne <amera.alleyne@gmail.com>
2022-04-04 21:49:00 -0700

    assertEqual() function

---
+++ assertEqual.js
@@ -0,0 +1,27 @@
+// Template literal example
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
+    // console.log("Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log(` 🛑🛑🛑 Assertion Failed ${actual} !== ${expected} `);
+
+    // console.log("Assertion Failed: " +  actual + " !== " + expected);
+  }
+
+};
+
+assertEqual("Lghthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+/*
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  }else{
+    console.log("Assertion Failed: " +  actual + " !== " + expected);
+  }
+
+};
+
+*/
