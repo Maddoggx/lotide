4ecb4b549acd80548b0b0deb3110ba059e17ec38
Amera Alleyne <amera.alleyne@gmail.com>
2022-04-05 13:21:44 -0700

    Implementing Tail

---
+++ tail.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+   
+    console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
+    
+  } else {
+   
+   console.log(` 🛑🛑🛑 Assertion Failed ${actual} !== ${expected} `);
+  }
+
+};
+const tail = (arr) => {
+  return arr.slice(1)
+}
+/*
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+const result = tail(words);
+ // no need to capture the return value since we are not checking it
+assertEqual(result.length, 2); // original array should still have 3 element
+console.log(result);
+*/
+
+const colour = ["blue", "yellow", "green", "red"];
+const resultOne = tail(colour);
+
+console.log(resultOne);
+assertEqual(resultOne.length, 3);
