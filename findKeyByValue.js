5d00c7cc8d4372092df28309a243984282614193
Amera Alleyne <amera.alleyne@gmail.com>
2022-04-09 17:58:04 -0700

    find key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+   
+    console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
+    // console.log("Assertion Passed: " + actual + " === " + expected);
+  } else {
+   
+    console.log(` 🛑🛑🛑 Assertion Failed ${actual} !== ${expected} `);
+
+    // console.log("Assertion Failed: " +  actual + " !== " + expected);
+  }
+};
+const findKeyByValue = function(object, value) {
+  return Object.keys(object).find(key => object[key] === value);
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+//console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama" === true);
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
