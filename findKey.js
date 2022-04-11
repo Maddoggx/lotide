89b3a19b994b013592395da57587039a2f5ccaab
Amera Alleyne <amera.alleyne@gmail.com>
2022-04-11 13:04:29 -0700

    findKey

--- findKey.js
+++ findKey.js
@@ -10,21 +10,25 @@ const assertEqual = (actual, expected) => {
 
 };
 
-const findKey = function() {
+// takes in an object and a callback.
+// it should scan the object and return the first key for which the callback returns a truthy value.
+// if no key is found then it should return undefined.
+const findKey = function(object, callback) {
+  for (let key in object) { // foor.. in loop to loop through keys of object.
+    // console.log(key);
+    if (callback(object[key])) {
+      //console.log(key);
+      return key;
+    }
+    // return undefined;
+  }
+};
 
-
-}
-
-
-
-
-
-
-findKey({
+console.log(findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+}, x => x.stars === 3));
