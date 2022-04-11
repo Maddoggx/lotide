914f4efa927cef1c595c1077a952971e1d5ae3c9
Amera Alleyne <amera.alleyne@gmail.com>
2022-04-11 13:10:36 -0700

    assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
+    // console.log("Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log(` 🛑🛑🛑 Assertion Failed ${actual} !== ${expected} `);
+
+    // console.log("Assertion Failed: " +  actual + " !== " + expected);
+  }
+
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
